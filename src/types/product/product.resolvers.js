d1fcfa78c0d2a2371767b8df0a4cf94d304c2ef8
Larry <larry@larryaball.net>
2019-08-21 16:48:28 -0500

    lesson-3

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,6 +9,57 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+/** product */
+const product = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+
+  const createdBy = ctx.user._id
+  return Product.create({ ...args.input, createdBy })
+}
+
+const products = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+
+  return Product.find({})
+    .lean()
+    .exec()
+}
+
+const updateProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+
+  const update = args.input
+  return Product.findByIdAndUpdate(args.id, update, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
   Query: {},
   Mutation: {},
